19cd2562f0e07709d2be3391eec7078fbb1d123b
margotduek <margotduek@gmail.com>
2016-05-11 17:51:01 -0500

    :rocket: home and footer ready

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,44 @@
+<footer class="footer">
+  <div class="container footer-gray">
+    <div class="menu">
+      <ul class="nav navbar-nav">
+        <li><a href="#">qiénes somos</a></li>
+        <li><a href="#">nuestros servicios</a></li>
+        <li><a href="#">faq</a></li>
+        <li><a href="#">contacto</a></li>
+        <li><a href="#">cotiza</a></li>
+      </ul>
+    </div>
+    <div class="rights">
+      <div class="col-xs-3">
+        <p>
+          @2015dinkbit <br> todos los derechos reservados
+        </p>
+      </div>
+      <div class="col-xs-3">
+        <p>
+          Avisos de privacidad
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="footer-blue">
+    <div class="blue-footer">
+      <div class="contact">
+        <p>
+          ¿Preguntas?|contacto@dinkbit.com|2224 1607
+        </p>
+      </div>
+      <div class="footer-followus">
+        <ul>
+          <li>siguenos en:</li>
+          <li><a href="https://www.facebook.com/dinkbit/?fref=ts"><img src="images/face.png" alt="" /></a></li>
+          <li><a href="https://twitter.com/dinkbit"><img src="images/twitter.png" alt="" /></a></li>
+          <li><a href="https://www.linkedin.com/company/dinkbit"><img src="images/in.png" alt="" /></a></li>
+          <li><a href="http://dinkbit.com/"><img src="images/logo.png" alt="" /></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,20 +1,137 @@
 @extends('layout.default')
 @section('content')
-
-<div class="logo-container">
-  <div class="layer">
-    <div class="logo-container-text">
-      <p>
-        lorem Ipsum
-      </p>
-      <h3>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-        ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
-        parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,
-        pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec
-        pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-      </h3>
+<section id="header">
+  <div class="logo-container">
+    <div class="layer">
+      <div class="logo-container-text">
+        <p>
+          lorem Ipsum
+        </p>
+        <h3>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+          ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
+          parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,
+          pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec
+          pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+        </h3>
+      </div>
     </div>
   </div>
-</div>
+</section id="logos" >
+<section>
+  <div class="general-text central">
+    <h3>lorem impsum titulum</h3>
+    <p>
+      Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos
+      de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias
+       desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la
+      imprenta) desconocido usó una galería de textos y los mezcló de tal manera
+      que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años,
+      sino que tambien ingresó como texto de relleno en documentos electrónicos,
+      quedando esencialmente igual al original.
+    </p>
+    <div class="col-sm-4 circle-size">
+      <div class="circle blue">
+        <img src="images/cotiza.png" alt="" />
+      </div>
+      <p>
+        Lorem ipsum
+      </p>
+    </div>
+    <div class="col-sm-4 circle-size">
+      <div class="circle light-blue">
+        <img src="images/clock.png" alt="" />
+      </div>
+      <p>
+        Lorem ipsum
+      </p>
+    </div>
+    <div class="col-sm-4 circle-size">
+      <div class="circle yellow">
+        <img src="images/money.png" alt="" />
+      </div>
+      <p>
+        Lorem ipsum
+      </p>
+    </div>
+  </div>
+</section>
+<section>
+  <div class="gray">
+    <div class="general-text general-text-b central">
+      <h3>¿por qué usar lorem impsum?</h3>
+      <div class="col-sm-4">
+        <h4>primero</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+          ligula eget dolor.
+        </p>
+      </div>
+      <div class="col-sm-4">
+        <h4>segundo</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+          ligula eget dolor.
+        </p>
+      </div>
+      <div class="col-sm-4">
+        <h4>tercero</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+          ligula eget dolor.
+        </p>
+      </div>
+      <div class="col-sm-4">
+        <h4>cuarto</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+          ligula eget dolor.
+        </p>
+      </div>
+      <div class="col-sm-4">
+        <h4>quinto</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+          ligula eget dolor.
+        </p>
+      </div>
+      <div class="col-sm-4">
+        <h4>sexto</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+          ligula eget dolor.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+<section>
+  <div class="general-text general-text-b central-b benefits">
+    <h3>beneficios de usar lorem ipsum</h3>
+    <br>
+    <ul>
+      <li>
+        <p>
+          Lorem Ipsum es simplemente el texto de relleno de las imprentas y
+          archivos de texto.
+        </p>
+      </li>
+      <li>
+        <p>
+          Lorem Ipsum ha sido el texto de relleno estándar de las industrias
+          desde el año 1500, cuando un impresor (N. del T. persona que se dedica
+          a laimprenta) desconocido usó una galería de textos y los mezcló de
+          tal manera que logró hacer un libro de textos especimen.
+        </p>
+      </li>
+      <li>
+        <p>
+          No sólo sobrevivió 500 años, sino que tambien ingresó como texto de
+          relleno en documentos electrónicos, quedando esencialmente igual al original.
+        </p>
+      </li>
+    </ul>
+    <button type="quote" class="btn btn-success center-block">Cotiza</button>
+  </div>
+</section>
 
 @stop
